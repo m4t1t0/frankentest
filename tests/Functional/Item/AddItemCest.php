12548d73be03ac2edb55f43634f88c4305aa6ade
Rafael Matito <rafa.matito@gmail.com>
2024-04-07 19:14:50 +0200

    feat: AddItem tests

--- tests/Functional/Item/AddItemCest.php
+++ tests/Functional/Item/AddItemCest.php
@@ -12,14 +12,20 @@ final class AddItemCest
     public function seeItemCreated(FunctionalTester $I): void
     {
         $itemId = 'c9d3de86-6c2e-42a1-9c87-86dcae10bfe5';
-        $I->haveHttpHeader('Content-Type', 'application/json');
-        $I->sendPost('/api/item', [
+        $eventType = 'app.item.core.aggregate.item.events.v1.item_was_added';
+        $payload = [
             'id' => $itemId,
-	        'name' => 'Prueba 1',
-	        'description' => 'Primera prueba',
+            'name' => 'Prueba 1',
+            'description' => 'Primera prueba',
             'quantity' => 5,
             'price' => 10.95
-        ]);
+        ];
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPost('/api/item', $payload);
+
         $I->seeResponseCodeIs(HttpCode::ACCEPTED);
+        $I->seeEventExists($itemId, $eventType);
+        $I->seeInReadDatabase('all_items', $itemId);
     }
 }
---
+++ tests/Support/Helper/DbHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Support\Helper;
+
+use Redis;
+use Codeception\Lib\ModuleContainer;
+use \Codeception\Module;
+use \Codeception\TestInterface;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+
+class DbHelper extends Module
+{
+    private Redis $redisWrite;
+    private Redis $redisRead;
+
+    public function __construct(ModuleContainer $moduleContainer, ?array $config = null)
+    {
+        $this->redisWrite = RedisAdapter::createConnection($config['connections']['writes']['dsn']);
+        $this->redisRead = RedisAdapter::createConnection($config['connections']['reads']['dsn']);
+        parent::__construct($moduleContainer, $config);
+    }
+
+    public function _before(TestInterface $test): void
+    {
+        $this->flushAll();
+    }
+
+    public function flushAll(): void
+    {
+        $this->redisWrite->flushAll();
+    }
+
+    public function seeEventExists(string $aggregateRootId, string $type): bool
+    {
+        $key = 'es_events_' . $aggregateRootId;
+
+        if (!$this->redisWrite->exists($key)) {
+            return false;
+        }
+
+        $events = $this->redisWrite->lrange($key, 0, -1);
+        foreach ($events as $event) {
+            $decodedEvent = json_decode($event, true);
+
+            if ($decodedEvent['headers']['__event_type'] === $type) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function seeInReadDatabase(string $prefix, string $aggregateRootId): bool
+    {
+        return (bool)$this->redisRead->exists($prefix . '_' . $aggregateRootId);
+    }
+}
